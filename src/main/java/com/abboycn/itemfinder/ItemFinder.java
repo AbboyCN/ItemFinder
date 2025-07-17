9b5d20507dea8a5c59d39039e5fb34e9e29b949e
Ab_boy_CN <15711389858@163.com>
2025-07-18 00:42:09 +0800

    Mod Info Updated

--- src/main/java/com/abboycn/itemfinder/ItemFinder.java
+++ src/main/java/com/abboycn/itemfinder/ItemFinder.java
@@ -19,6 +19,6 @@ public class ItemFinder implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Successfully Loaded ItemFinder Mod!");
 	}
 }
